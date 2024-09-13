15c250ab24b229764d5947d647e30e09dc391401
Ryan James <ryanjames1729@gmail.com>
2024-09-13 10:34:03 -0400

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+// Resource Finder Starter Code              A+CR
+class Main {
+  public static void main(String[] args) {
+    Measure world = new Measure();
+		
+    for(int r = 0; r < 30; r++){
+      for(int c = 0; c < 20; c++){
+        // Write your code here.
+        
+       
+        // This marks the end of your code segment.
+      }
+    } 
+     /* this code allows the program to finish displaying all the values to the console
+     you must press the STOP button to stop the program from running */
+     while (true);    
+  }
+}
