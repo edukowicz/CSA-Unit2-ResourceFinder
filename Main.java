164711b7a731e696845b1d3621ac374b00ec7d0f
edukowicz <26.ellie.dukowicz@carolinaday.org>
2024-09-17 18:40:01 +0000

    final project

--- Main.java
+++ Main.java
@@ -5,14 +5,20 @@ class Main {
 		
     for(int r = 0; r < 30; r++){
       for(int c = 0; c < 20; c++){
-        // Write your code here.
-        
-       
-        // This marks the end of your code segment.
+        double density = world.getMaterial(r, c);         // here i set the variable "density" to the number (of the density) in row "r" and column "c"
+        density = density * 100;
+        density = density + 0.5;
+        int x = (int)density;
+        double rounded = (double)x/100;                   //this line concludes the operations used to 
+        if ((rounded >= 1.77) && (rounded <= 3.2)){
+          System.out.println("Row " + r + " and column " + c + " have a sedimentary rock with a density of " + rounded);
+          System.out.println("");
+        }
+        if ((rounded >= 0.35) && (rounded <= 0.55)){
+          System.out.println("Row " + r + " and column " + c + " have unpacked snow with a density of " + rounded);
+          System.out.println("");
+        }
       }
     } 
-     /* this code allows the program to finish displaying all the values to the console
-     you must press the STOP button to stop the program from running */
-     while (true);    
   }
 }
