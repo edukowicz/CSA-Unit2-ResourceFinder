42b1f4690b452c7bf7a175f538d2d2b01c195ed9
edukowicz <26.ellie.dukowicz@carolinaday.org>
2024-09-17 18:54:33 +0000

    final project

--- Main.java
+++ Main.java
@@ -9,12 +9,12 @@ class Main {
         density = density * 100;
         density = density + 0.5;
         int x = (int)density;
-        double rounded = (double)x/100;                   //this line concludes the operations used to 
-        if ((rounded >= 1.77) && (rounded <= 3.2)){
+        double rounded = (double)x/100;                   // this line concludes the operations used to round the density to the hundredths place
+        if ((rounded >= 1.77) && (rounded <= 3.2)){.      // here, the code checks if the density is within the range of the densities of sedinmentary rocks; if it is, the code prints a statement  
           System.out.println("Row " + r + " and column " + c + " have a sedimentary rock with a density of " + rounded);
           System.out.println("");
         }
-        if ((rounded >= 0.35) && (rounded <= 0.55)){
+        if ((rounded >= 0.35) && (rounded <= 0.55)){.     // here, the code checks if the density is within the range of the densities of unpacked snow; if it is, then the code pritns a statement
           System.out.println("Row " + r + " and column " + c + " have unpacked snow with a density of " + rounded);
           System.out.println("");
         }
